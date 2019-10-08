0659bd9141dc0b702ce77870b18e7d883a8ef13f
Arthur Milchior <Arthur@Milchior.fr>
2019-10-08 15:20:30 +0200

    import _

--- quick_tagging21.py
+++ quick_tagging21.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from anki.lang import _
 from aqt import mw
 from aqt.utils import getTag, getText, tooltip
 from aqt.reviewer import Reviewer
